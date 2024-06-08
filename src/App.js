e82a304b263d7dd39c4131aecb655dd1a88a3bd5
SheikhMujib <mbasheikhmujib@gmail.com>
2024-06-08 15:54:18 +0600

    Assignment 12 started

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+    </div>
+  );
+}
+
+export default App;
