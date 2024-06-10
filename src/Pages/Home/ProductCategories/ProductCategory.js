02b4c3c5136319156cb69a2e5ae0227632eec430
SheikhMujib <mbasheikhmujib@gmail.com>
2024-06-10 23:13:18 +0600

    Login form created

--- src/Pages/Home/ProductCategories/ProductCategory.js
+++ src/Pages/Home/ProductCategories/ProductCategory.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const ProductCategory = ({card}) => {
-  const {name, productsCount, img} = card;
-    return (
-        <div className="card  bg-base-100 shadow-xl">
-  <figure>
-    <img  src={img} alt="smartphones" className=''/></figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{name}</h2>
-    <p>{productsCount} Products.</p>
-    <div className="card-actions">
-      <button className="btn btn-primary text-white">Browse Products</button>
+const ProductCategory = ({ card }) => {
+  const { name, productsCount, img } = card;
+  return (
+    <div className="card  bg-base-100 shadow-xl">
+      <figure>
+        <img src={img} alt="smartphones" className="" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{productsCount} Products.</p>
+        <div className="card-actions">
+          <button className="btn btn-primary text-white">
+            Browse Products
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default ProductCategory;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const {register, handleSubmit} = useForm();
+    
+    const handleLogin = data => {
+      console.log(data);
+    }
+
     return (
-        <div>
-            <h2>This is login page.</h2>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row px-24">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body" onSubmit={handleSubmit(handleLogin)}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} className="input input-bordered" required/>
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="Password" {...register("Password", {required: true, max: 0, min: 7, maxLength: 99})} className="input input-bordered" required/>
+          <label className="label">
+            <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <input type="submit" className="btn btn-primary text-white" value="Login"/>
+        </div>
+      </form>
+        <div className='pb-7'>
+        <p className='text-center'>New to Resale Repo? <Link to="/" className='text-secondary'>Create an account</Link></p>
+        <div className="divider">OR</div>
+        <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+        </div>
+    </div>
+  </div>
+</div>
     );
 };
 
